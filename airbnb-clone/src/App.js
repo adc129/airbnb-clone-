14c93b2a78d80981529240c54ad347d2dc2542ce
adc129 <60369328+adc129@users.noreply.github.com>
2020-10-25 19:54:03 -0500

    header

---
+++ airbnb-clone/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Home from './Home.js'
+import Header from './Header.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Home />
+    {/*home*/}
+      {/*header*/}
+      {/*banner*/}
+      {/*cards*/}
+      {/*footer*/}
+      {/*search page*/}
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ airbnb-clone/src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from "@material-ui/core";
+
+
+function Header() {
+  return (
+      <div className='header'>
+        <img
+          className="header__icon"
+          src="https://logos-download.com/wp-content/uploads/2016/03/Airbnb_logo_small.png"
+          alt=""
+        />
+        <div className='header__center'>
+          <input type ="text" />
+          <SearchIcon />
+        </div>
+        <div className='header__right'>
+          <p>Become a host</p>
+          <LanguageIcon />
+          <ExpandMoreIcon />
+          <Avatar />
+
+        </div>
+
+
+
+
+      </div>
+  )
+
+
+}
+export default Header
